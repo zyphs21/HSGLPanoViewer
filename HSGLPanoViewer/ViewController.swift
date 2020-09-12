ea6466da441e0d59a865800d2963ec46cb2df159
zyphs21 <hansenhs21@live.com>
2020-09-12 11:19:49 +0800

    Setup Project

---
+++ HSGLPanoViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HSGLPanoViewer
+//
+//  Created by Hanson on 2020/9/12.
+//  Copyright © 2020 Hanson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showGLKitPano(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showGLSLPano(_ sender: Any) {
+        
+    }
+    
+}
+
